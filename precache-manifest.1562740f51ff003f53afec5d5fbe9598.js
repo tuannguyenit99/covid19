429122b4a9fa3e15af550d803bcd4d875267d8ef
Nguyễn Quốc Tuấn <76480614+tuannguyenit99@users.noreply.github.com>
2021-10-09 17:45:16 +0700

    Updates

---
+++ precache-manifest.1562740f51ff003f53afec5d5fbe9598.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09fd9cc438bab8b3d98a5d08f0685fe3",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "f06580d1978c5063dc85",
+    "url": "/covid19/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "1daa83dd7db74b3435e0",
+    "url": "/covid19/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "f06580d1978c5063dc85",
+    "url": "/covid19/static/js/2.29863f4f.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid19/static/js/2.29863f4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1daa83dd7db74b3435e0",
+    "url": "/covid19/static/js/main.a948d0f0.chunk.js"
+  },
+  {
+    "revision": "7d1038710bb674affb6b",
+    "url": "/covid19/static/js/runtime-main.367779e9.js"
+  }
+]);
